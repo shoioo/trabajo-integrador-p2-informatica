e0ff30e159f72cdd63f74195bb784874005c9aa1
Shaila Hussein <shaila.hussein@outlook.com>
2025-11-07 20:37:09 -0300

    shaila: agregué los datos solicitados al inicio.

---
+++ integrador-grupo-2.c
@@ -0,0 +1,9 @@
+/*
+•	Grupo N° 2.
+•	Carrera: Ing. en Computación.
+•	Apellido y nombre de los integrantes:
+    Hussein, Shaila Verenice
+    Navas, Lucrecia
+    Paul, Constanza
+    Prado Leggio, Laureano
+*/
